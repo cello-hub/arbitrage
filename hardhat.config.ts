8c79c98a2d62c9b63c42a70cd39d2f30464f2b4a
benson <gwanbit@gmail.com>
2023-05-26 12:00:40 +0800

    init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,33 @@
 import { type HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+import 'dotenv/config'
+
+const bscRpc = 'https://bsc-dataseed1.defibit.io/'
+const bscTestnetRpc = 'https://data-seed-prebsc-1-s1.binance.org:8545/'
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.18'
+  solidity: '0.8.20',
+  networks: {
+    hardhat: {
+      forking: {
+        url: bscRpc,
+        enabled: true
+      },
+      accounts: {
+        accountsBalance: '1000000000000000000000000'
+      }
+    },
+    bscTestnet: {
+      url: bscTestnetRpc,
+      chainId: 0x61,
+      accounts: [process.env.private]
+    },
+    bsc: {
+      url: bscRpc,
+      chainId: 0x38,
+      accounts: [process.env.private]
+    }
+  }
 }
 
 export default config
